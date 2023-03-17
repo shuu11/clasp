e337b92fe4fc18a3245003403ec64cdd3199acd5
shuu11 <shuu1104main@gmail.com>
2023-03-17 15:07:59 +0900

    :poop:

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,6 @@
  * どうしてもファイル分割したい場合はwebpack使う
  */
 
-type App = GoogleAppsScript.Spreadsheet.Spreadsheet;
-type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
-type Range = GoogleAppsScript.Spreadsheet.Range;
-
-export default class MyGas {
-	#ss: App = SpreadsheetApp.getActiveSpreadsheet();
-
-	constructor(public name: string) {}
-
-	#getSheet(): Sheet | null {
-		return this.#ss.getSheetByName(this.name);
-	}
-
-	#getRange(row: number, column: number): Range | null {
-		return this.#getSheet()?.getRange(row, column) || null;
-	}
-
-	getValue(row: number, column: number): unknown {
-		return this.#getRange(row, column)?.getValue();
-	}
-
-	getLastRow(): number | null {
-		return this.#getSheet()?.getLastRow() || null;
-	}
-
-	getLastColumn(): number | null {
-		return this.#getSheet()?.getLastColumn() || null;
-	}
-
-	setValue(row: number, column: number, value: unknown): void {
-		this.#getRange(row, column)?.setValue(value);
-	}
-}
-
 /**
  * my app
  */
@@ -45,5 +11,5 @@ const endRow = myGas.getLastRow();
 const endColumn = myGas.getLastColumn();
 
 function main() {
-	console.log(endRow, endColumn);
+	console.log(endColumn, endRow);
 }
